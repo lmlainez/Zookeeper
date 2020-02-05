0af74a3f643e1492eef4ae672825fd428fe4044a
Luis Miguel Lainez Reche <Luis.Lainez@compareinsurance.com.au>
2020-02-05 16:52:23 +1100

    Starting first exercise

---
+++ LeaderElection/src/main/java/LeaderElection.java
@@ -0,0 +1,16 @@
+import org.apache.zookeeper.ZooKeeper;
+
+public class LeaderElection {
+
+    private static final String ZOOKEPER_ADDRESS = "localhost:2181";
+    private ZooKeeper zookeeper;
+
+    public static void main(String[] args){
+
+    }
+
+    public void connectToZookeeper(){
+
+    }
+
+}
